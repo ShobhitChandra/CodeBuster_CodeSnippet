73026d65e99b61b4d36622a56dd35dabb7e51d52
Pransu2005 <161361849+Pransu2005@users.noreply.github.com>
2024-04-07 23:27:51 +0530

    Update CodeSnippetManager.cpp

--- CodeSnippetManager.cpp
+++ CodeSnippetManager.cpp
@@ -1,7 +1,14 @@
 #include "CodeSnippetManager.h"
 #include <iostream>
+#include <string>
+#include <cstring>
 #include <fstream>
 #include <filesystem>
+// #include <experimental/filesystem>
+
+// namespace fs = std::experimental::filesystem;
+namespace fs = std::filesystem;
+using namespace std;
 
 CodeSnippetManager::CodeSnippetManager(const string &prefix) : filePrefix(prefix)
 {
@@ -14,7 +21,6 @@ CodeSnippetManager::~CodeSnippetManager()
     SaveToFile();
     SaveInvertedIndex();
 }
-
 
 bool CodeSnippetManager::AddSnippet(const string &tag, const string &code)
 {
@@ -31,6 +37,22 @@ bool CodeSnippetManager::AddSnippet(const string &tag, const string &code)
     SaveToFile();
     SaveInvertedIndex();
     return true;
+}
+
+bool CodeSnippetManager::RetrieveSnippet(const string &tag)
+{
+    auto it = snippets.find(tag);
+    if (it != snippets.end())
+    {
+        cout << "Snippets for tag is: " << tag << ":\n"
+             << it->second << endl;
+        return true;
+    }
+    else
+    {
+        cout << "No snippets found for tag: " << tag << "." << endl;
+        return false;
+    }
 }
 
 bool CodeSnippetManager::DeleteSnippet(const string &tag)
@@ -68,150 +90,6 @@ bool CodeSnippetManager::DeleteSnippet(const string &tag)
         cout << "No snippet found for tag: " << tag << endl;
         return false;
     }
-}
-void CodeSnippetManager::RemoveFromInvertedIndex(const string &tag, const string &code)
-{
-    istringstream iss(code);
-    string word;
-    while (iss >> word)
-    {
-        auto it = invertedIndex.find(word);
-        if (it != invertedIndex.end())
-        {
-            it->second.erase(tag);
-            if (it->second.empty())
-            {
-                invertedIndex.erase(it);
-            }
-        }
-    }
-}
-bool CodeSnippetManager::RetrieveSnippet(const string &tag)
-{
-    auto it = snippets.find(tag);
-    if (it != snippets.end())
-    {
-        cout << "Snippets for tag is: " << tag << ":\n"
-             << it->second << endl;
-        return true;
-    }
-    else
-    {
-        cout << "No snippets found for tag: " << tag << "." << endl;
-        return false;
-    }
-}
-
-bool CodeSnippetManager:: Save_to_file(const string& filename)
-{
-    for (const auto &entry : snippets)
-    {
-        string filename = filePrefix + entry.first + ".txt";
-        ofstream outFile(filename);
-        if (!outFile)
-        {
-            cerr << "Error opening file: " << filename << endl;
-            return false;
-        }
-        outFile << entry.second;
-        outFile.close();
-    }
-    return true;
-}
-
-bool CodeSnippetManager:: Load_to_file(const string& filename)
-{
-    snippets.clear();
-    for (const auto &entry : fs::directory_iterator("./snippets/"))
-    {
-        if (entry.path().extension() == ".txt")
-        {
-            string filename = entry.path().filename().string();
-            string tag = filename.substr(0, filename.length() - 4);
-            ifstream inFile(entry.path());
-            if (!inFile)
-            {
-                cerr << "Error opening file: " << filename << endl;
-                return false;
-            }
-            string code((istreambuf_iterator<char>(inFile)), istreambuf_iterator<char>());
-            snippets[tag] = code;
-            inFile.close();
-        }
-    }
-    return true;
-}
-
-void CodeSnippetManager::LoadInvertedIndex()
-{
-    ifstream file("inverted_index.txt");
-    if (file.is_open())
-    {
-        string line;
-        while (getline(file, line))
-        {
-            istringstream iss(line);
-            string word;
-            if (iss >> word)
-            {
-                string tag;
-                while (iss >> tag)
-                {
-                    invertedIndex[word].insert(tag);
-                }
-            }
-        }
-        file.close();
-    }
-}
-
-void CodeSnippetManager::SaveInvertedIndex()
-{
-    ofstream file("inverted_index.txt");
-    if (file.is_open())
-    {
-        for (const auto &entry : invertedIndex)
-        {
-            const string &word = entry.first;
-            const unordered_set<string> &tags = entry.second;
-            file << word;
-            for (const string &tag : tags)
-            {
-                file << " " << tag;
-            }
-            file << endl;
-        }
-        file.close();
-    }
-}
-
-vector<string> CodeSnippetManager::SearchSnippets(const string &tagPrefix)
-{
-    vector<string> closestTags;
-    unordered_set<string> addedTags;
-    stringstream ss(tagPrefix);
-    string word;
-    while (ss >> word)
-    {
-        for (const auto &entry : snippets)
-        {
-            const string &tag = entry.first;
-            if (tag.substr(0, word.length()) == word && addedTags.find(tag) == addedTags.end())
-            {
-                if (word == "a" && word == "am")
-                {
-                    continue;
-                }
-                else
-                {
-                    closestTags.push_back(tag);
-                    addedTags.insert(tag);
-                }
-            }
-        }
-    }
-
-    return closestTags;
 }
 
 bool CodeSnippetManager::UpdateSnippet(const string &tag, const string &code)
@@ -258,14 +136,73 @@ bool CodeSnippetManager::UpdateSnippet(const string &tag, const string &code)
     }
 }
 
-void CodeSnippetManager::UpdateInvertedIndex(const string &tag, const string &code)
+vector<string> CodeSnippetManager::SearchSnippetsByTag(const string &tagPrefix)
 {
-    istringstream iss(code);
+    vector<string> closestTags;
+    unordered_set<string> addedTags;
+    stringstream ss(tagPrefix);
     string word;
-    while (iss >> word)
+    while (ss >> word)
     {
-        invertedIndex[word].insert(tag);
+        for (const auto &entry : snippets)
+        {
+            const string &tag = entry.first;
+            if (tag.substr(0, word.length()) == word && addedTags.find(tag) == addedTags.end())
+            {
+                if (word == "a" && word == "am")
+                {
+                    continue;
+                }
+                else
+                {
+                    closestTags.push_back(tag);
+                    addedTags.insert(tag);
+                }
+            }
+        }
     }
+
+    return closestTags;
+}
+
+bool CodeSnippetManager::SaveToFile()
+{
+    for (const auto &entry : snippets)
+    {
+        string filename = filePrefix + entry.first + ".txt";
+        ofstream outFile(filename);
+        if (!outFile)
+        {
+            cerr << "Error opening file: " << filename << endl;
+            return false;
+        }
+        outFile << entry.second;
+        outFile.close();
+    }
+    return true;
+}
+
+bool CodeSnippetManager::LoadFromFile()
+{
+    snippets.clear();
+    for (const auto &entry : fs::directory_iterator("./snippets/"))
+    {
+        if (entry.path().extension() == ".txt")
+        {
+            string filename = entry.path().filename().string();
+            string tag = filename.substr(0, filename.length() - 4);
+            ifstream inFile(entry.path());
+            if (!inFile)
+            {
+                cerr << "Error opening file: " << filename << endl;
+                return false;
+            }
+            string code((istreambuf_iterator<char>(inFile)), istreambuf_iterator<char>());
+            snippets[tag] = code;
+            inFile.close();
+        }
+    }
+    return true;
 }
 
 vector<string> CodeSnippetManager::SearchSnippetsByContent(const vector<string> &keywords)
@@ -282,4 +219,73 @@ vector<string> CodeSnippetManager::SearchSnippetsByContent(const vector<string> 
     return vector<string>(matchingTags.begin(), matchingTags.end());
 }
 
+void CodeSnippetManager::LoadInvertedIndex()
+{
+    ifstream file("inverted_index.txt");
+    if (file.is_open())
+    {
+        string line;
+        while (getline(file, line))
+        {
+            istringstream iss(line);
+            string word;
+            if (iss >> word)
+            {
+                string tag;
+                while (iss >> tag)
+                {
+                    invertedIndex[word].insert(tag);
+                }
+            }
+        }
+        file.close();
+    }
+}
 
+void CodeSnippetManager::SaveInvertedIndex()
+{
+    ofstream file("inverted_index.txt");
+    if (file.is_open())
+    {
+        for (const auto &entry : invertedIndex)
+        {
+            const string &word = entry.first;
+            const unordered_set<string> &tags = entry.second;
+            file << word;
+            for (const string &tag : tags)
+            {
+                file << " " << tag;
+            }
+            file << endl;
+        }
+        file.close();
+    }
+}
+
+void CodeSnippetManager::UpdateInvertedIndex(const string &tag, const string &code)
+{
+    istringstream iss(code);
+    string word;
+    while (iss >> word)
+    {
+        invertedIndex[word].insert(tag);
+    }
+}
+
+void CodeSnippetManager::RemoveFromInvertedIndex(const string &tag, const string &code)
+{
+    istringstream iss(code);
+    string word;
+    while (iss >> word)
+    {
+        auto it = invertedIndex.find(word);
+        if (it != invertedIndex.end())
+        {
+            it->second.erase(tag);
+            if (it->second.empty())
+            {
+                invertedIndex.erase(it);
+            }
+        }
+    }
+}
