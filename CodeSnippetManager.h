9ec1db4dc79c784e11e89fd4ee614aa690d6879b
Pransu2005 <161361849+Pransu2005@users.noreply.github.com>
2024-04-07 23:23:11 +0530

    Update CodeSnippetManager.h

--- CodeSnippetManager.h
+++ CodeSnippetManager.h
@@ -7,30 +7,31 @@
 #include <unordered_map>
 #include <unordered_set>
 #include <sstream>
+using namespace std;
 
 class CodeSnippetManager
 {
 private:
-  std::map<std::string, std::string> snippets;
-  std::string filePrefix;
-  std::unordered_map<std::string, std::unordered_set<std::string>> invertedIndex;
+  map<string,string> snippets;
+  string filePrefix;
+  unordered_map<string, unordered_set<string>> invertedIndex;
 
 public:
-  CodeSnippetManager(const std::string &prefix = "./snippets/");
+  CodeSnippetManager(const string &prefix = "./snippets/");
   ~CodeSnippetManager();
 
-  bool AddSnippet(const std::string &tag, const std::string &code);
-  bool RetrieveSnippet(const std::string &tag);
-  bool DeleteSnippet(const std::string &tag);
-  bool UpdateSnippet(const std::string &tag, const std::string &code);
-  std::vector<std::string> SearchSnippetsByTag(const std::string &tagPrefix);
-  std::vector<std::string> SearchSnippetsByContent(const std::vector<std::string> &keywords);
+  bool AddSnippet(const string &tag, const string &code);
+  bool RetrieveSnippet(const string &tag);
+  bool DeleteSnippet(const string &tag);
+  bool UpdateSnippet(const string &tag, const string &code);
+  vector<string> SearchSnippetsByTag(const string &tagPrefix);
+  vector<string> SearchSnippetsByContent(const vector<string> &keywords);
   bool SaveToFile();
   bool LoadFromFile();
   void LoadInvertedIndex();
   void SaveInvertedIndex();
-  void UpdateInvertedIndex(const std::string &tag, const std::string &code);
-  void RemoveFromInvertedIndex(const std::string &tag, const std::string &code);
+  void UpdateInvertedIndex(const string &tag, const string &code);
+  void RemoveFromInvertedIndex(const string &tag, const string &code);
 };
 
 #endif
