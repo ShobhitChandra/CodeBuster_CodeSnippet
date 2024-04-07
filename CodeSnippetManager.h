6e990ca8dd10ebd1e19dfaa9c105ef3f11dae9d7
karthikraman2006 <161368844+karthikraman2006@users.noreply.github.com>
2024-04-07 11:36:27 +0530

    Update CodeSnippetManager.h

--- CodeSnippetManager.h
+++ CodeSnippetManager.h
@@ -1,6 +1,5 @@
 #ifndef CODE_SNIPPET_MANAGER_H
-#define CODE_SNIPPER_MANAGER_H
-using namespace std;
+#define CODE_SNIPPET_MANAGER_H
 
 #include <string>
 #include <map>
@@ -9,32 +8,29 @@ using namespace std;
 #include <unordered_set>
 #include <sstream>
 
-class CodeSnippetManager{
-  private:
-  map<string,string> snippets;
-  string filePrefix;
-  unordered_map<string, unordered_set<string>> invertedIndex;
+class CodeSnippetManager
+{
+private:
+  std::map<std::string, std::string> snippets;
+  std::string filePrefix;
+  std::unordered_map<std::string, std::unordered_set<std::string>> invertedIndex;
 
-   public:
-    CodeSnippetManager(const string &prefix = "./snippets/");
+public:
+  CodeSnippetManager(const std::string &prefix = "./snippets/");
+  ~CodeSnippetManager();
 
-  void Add_Snippets(string& tag,const string& code);
-  void Retrieve_Snippets(const string& tag);
-  bool DeleteSnippet(const string &tag);
-  void RemoveFromInvertedIndex(const string &tag, const string &code);
-  bool Save_to_file(const string& filename);
-  bool Load_to_file(const string& filename);  
+  bool AddSnippet(const std::string &tag, const std::string &code);
+  bool RetrieveSnippet(const std::string &tag);
+  bool DeleteSnippet(const std::string &tag);
+  bool UpdateSnippet(const std::string &tag, const std::string &code);
+  std::vector<std::string> SearchSnippetsByTag(const std::string &tagPrefix);
+  std::vector<std::string> SearchSnippetsByContent(const std::vector<std::string> &keywords);
+  bool SaveToFile();
+  bool LoadFromFile();
   void LoadInvertedIndex();
   void SaveInvertedIndex();
-  vector<string> SearchSnippets(const string &tagPrefix);
-
-
-    
-
-
-
-
-
+  void UpdateInvertedIndex(const std::string &tag, const std::string &code);
+  void RemoveFromInvertedIndex(const std::string &tag, const std::string &code);
 };
 
 #endif
